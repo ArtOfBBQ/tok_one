0bd681b47382b35e7fc27cf149c1ecec4320bbb0
ArtofBBQ <jellevandeneynde@Jelles-iMac.local>
2022-10-25 17:40:43 +0900

    app termination for iphone

--- src/ios/AppDelegate.h
+++ src/ios/AppDelegate.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+#include "clientlogic.h"
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
+// - (void)applicationWillTerminate:(UIApplication *)application;
 @end
--- src/ios/AppDelegate.m
+++ src/ios/AppDelegate.m
@@ -11,4 +11,16 @@
 {
     return YES;
 }
+
+- (void)
+    applicationWillTerminate:(UIApplication *)application
+{
+    client_logic_shutdown();
+    
+    add_profiling_stats_to_log();
+    bool32_t write_succesful = false;
+    log_dump(&write_succesful);
+    log_append("ERROR - failed to store the log file on app termination..\n");
+}
+
 @end
