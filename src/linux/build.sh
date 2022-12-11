9f2a931691389d4bf75cca0c09a141e863dbaf0e
Jelle <osoraku@gmail.com>
2022-12-11 17:33:02 +0900

    making a window on linux (gotta start somehwere)

--- src/linux/build.sh
+++ src/linux/build.sh
@@ -1,36 +1,25 @@
 APP_NAME="hello3dgfx"
 PLATFORM="linux"
 
-TOK_ONE_SOURCEFILES="src/$PLATFORM/main.c src/$PLATFORM/platform_layer.c src/shared/draw_triangle.c src/shared/bitmap_renderer.c src/shared_windows_macos/platform_layer.c src/shared/texture_array.c src/shared/userinput.c src/shared/zpolygon.c src/shared/lightsource.c src/shared/software_renderer.c src/shared/window_size.c src/shared/debigulator/src/decode_png.c src/shared/debigulator/src/inflate.c src/shared/debigulator/src/decodedimage.c src/shared/clientlogic.c src/shared/common.c src/shared/text.c src/shared/scheduled_animations.c src/shared/texquad_type.c"
+COMPILER_OPTIONS="-std=c99 -I/usr/include/SDL2 -D_REENTRANT"
+LINKER_OPTIONS="-lSDL2"
 
 echo "Building $APP_NAME for $PLATFORM..."
 
 echo "deleting previous build(s)..."
-rm -r -f build/$PLATFORM/$APP_NAME.app/$APP_NAME
-rm -r -f build/$PLATFORM/$APP_NAME.app/$APP_NAME.dsym
+rm -r -f build/$PLATFORM
 
 echo "Creating build folder..."
 mkdir build
 mkdir build/$PLATFORM
-mkdir build/$PLATFORM/$APP_NAME.app
-
-# echo "skipping resource copy..."
-echo "copy resources..."
-cp resources/cardwithuvcoords.obj build/$PLATFORM/$APP_NAME.app/cardwithuvcoords.obj
-cp resources/teapot.obj build/$PLATFORM/$APP_NAME.app/teapot.obj
-cp resources/phoebus.png build/$PLATFORM/$APP_NAME.app/phoebus.png
-cp resources/sampletexture.png build/$PLATFORM/$APP_NAME.app/sampletexture.png
-cp resources/font.png build/$PLATFORM/$APP_NAME.app/font.png
-cp resources/structuredart1.png build/$PLATFORM/$APP_NAME.app/structuredart1.png
-cp resources/structuredart2.png build/$PLATFORM/$APP_NAME.app/structuredart2.png
-cp resources/structuredart3.png build/$PLATFORM/$APP_NAME.app/structuredart3.png
-cp resources/replacement.png build/$PLATFORM/$APP_NAME.app/replacement.png
 
 echo "Compiling & linking $APP_NAME..."
-sudo gcc -D LONGINT64 -Wall -std="c99" -g -o0 $TOK_ONE_SOURCEFILES -o build/$PLATFORM/$APP_NAME.app/$APP_NAME -lm
-
-echo "Booting $APP_NAME"
-# (cd build/$PLATFORM/$APP_NAME.app && ./$APP_NAME)
-# (cd build/$PLATFORM/$APP_NAME.app && valgrind --leak-check=full ./$APP_NAME)
-# (cd build/$PLATFORM/$APP_NAME.app && gdb ./$APP_NAME)
+if
+sudo gcc src/linux/main.c -o build/$PLATFORM/$APP_NAME $COMPILER_OPTIONS $LINKER_OPTIONS
+then
+echo "succesful compilation, running..."
+build/$PLATFORM/$APP_NAME
+else
+echo "build failed"
+fi
 
--- src/linux/main.c
+++ src/linux/main.c
@@ -1,78 +1,69 @@
 // functions we must implement
-#include "../shared/platform_layer.h" 
+// #include "../shared/platform_layer.h" 
 
 // shared functionality we can use
 #include "../shared/common.h"
-#include "../shared/userinput.h"
-#include "../shared/window_size.h"
-#include "../shared/vertex_types.h"
-#include "../shared/zpolygon.h"
-#include "../shared/software_renderer.h"
-#include "../shared/bitmap_renderer.h"
-#include "../shared/clientlogic.h"
+// #include "../shared/userinput.h"
+// #include "../shared/window_size.h"
+// #include "../shared/vertex_types.h"
+// #include "../shared/zpolygon.h"
+// #include "../shared/software_renderer.h"
+// #include "../shared/bitmap_renderer.h"
+// #include "../shared/clientlogic.h"
+
+#include <stdio.h>
+#include <SDL.h>
+
+static bool32_t handle_SDL_event(
+    SDL_Event * event)
+{
+    // do stuff!
+    switch (event->type)
+    {
+        case SDL_QUIT: {
+            return 1;
+        }
+        break;
+    }
+
+    return 0;
+}
 
 int main(int argc, const char * argv[]) 
 {
-    window_height = 750;
-    window_width = 1200;
-    bool32_t gameloop = 5000;
+    // extern DECLSPEC int SDLCALL SDL_ShowSimpleMessageBox(Uint32 flags, const char *title, const char *message, SDL_Window *window);
     
-    init_projection_constants();
-    init_renderer();
-
-    Vertex * vertices_for_gpu = malloc(sizeof(Vertex) * 5000);
-    
-    while (gameloop > 0) {
-        
-        printf("gameloop: %u\n", gameloop);
-        
-        // uint64_t time = platform_get_current_time_microsecs();
-        // uint64_t elapsed = time - previous_time;
-        uint64_t elapsed = 7500;
-
-        for (uint32_t i = 0; i < texture_arrays_size; i++) {
-            if (texture_arrays[i].request_update) {
-                // [self updateTextureArray: i];
-                break;
-            }
-        }
-        
-        uint32_t vertices_for_gpu_size = 0;
-
-        // translate all lights
-        zLightSource zlights_transformed[zlights_to_apply_size];
-        translate_lights(
-            /* originals: */ &zlights_to_apply[0],
-            /* out_translated: */ &zlights_transformed[0],
-            /* lights_count: */ zlights_to_apply_size);
-        
-        software_render(
-            /* next_gpu_workload: */
-                vertices_for_gpu,
-            /* next_gpu_workload_size: */
-                &vertices_for_gpu_size,
-            /* zlights_transformed: */
-                zlights_transformed,
-            /* elapsed_microseconds: */
-                elapsed);
-        
-        draw_texquads_to_render(
-            /* next_gpu_workload: */
-                vertices_for_gpu,
-            /* next_gpu_workload_size: */
-                &vertices_for_gpu_size,
-            /* zlights_transformed: */
-                zlights_transformed);
-
-        printf(
-            "if there was a UI we'd draw %u vertices\n",
-            vertices_for_gpu_size);
-        
-        gameloop--;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_ShowSimpleMessageBox(
+            /* Uint32 flags: */
+                SDL_MESSAGEBOX_INFORMATION,
+            /* const char *title: */
+                "Error",
+            /* const char *message: */
+                "Failed to initialize SDL",
+            /* SDL_Window *window: */
+                0);
+        return 0;
     }
-
-    free(vertices_for_gpu);
     
-    printf("end of 5000 gameloops\n");
+    SDL_Window * window = SDL_CreateWindow(
+        "TOK ONE",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_RESIZABLE);
+    
+   
+    while (1) {
+        SDL_Event event;
+        SDL_WaitEvent(&event);
+        if (handle_SDL_event(&event)) {
+            break;
+        }
+    }
+    
+    SDL_Quit();
+    return 1;
 }
 
