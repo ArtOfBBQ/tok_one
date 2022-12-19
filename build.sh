4c62f0f0535e83dc3b291b1f4eb16e714a20a948
ArtofBBQ <jellevandeneynde@Jelles-iMac.local>
2022-12-20 02:33:54 +0900

    add submodule reminder to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 # echo "Deleting buggy, useless, resource intensive and in all other ways awful Apple malware..."
 # rm -r /Users/jellevandeneynde/Library/Developer/Xcode/DerivedData
 
+if test -f "engine_src/shared/debigulator/src/decode_png.c";
+then
+echo "."
+else
+echo "Debigulator folder empty... Forgot to clone with submodules again?"
+echo "reminder: to download the submodule, try:"
+echo "cd engine_src/shared/debigulator"
+echo "git submodule update --init"
+exit 0
+fi
+
 # bash engine_src/linux/build.sh
 bash engine_src/macos/build.sh
 # bash engine_src/macos/buildtests.sh
